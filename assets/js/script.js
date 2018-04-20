a177f6df4a4e9a886920ecb3b5efe272bbcaa95e
irisperson <36912709+irisperson@users.noreply.github.com>
2018-04-19 21:36:54 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function()  {
+
+
+	$(".picture").click(function() {
+		$(this).addClass("pic-click");
+		$("#overlay").show();
+		$("#overlay").click(function(){
+			$(this).hide();
+			$(".picture").removeClass("pic-click");
+		})
+	});
+
+	$(".picture").click(function() {
+		$(this).addClass("pic-click");
+		$("#overlay").show();
+		$("#overlay").click(function(){
+			$(this).hide();
+			$(".picture").removeClass("pic-click");
+		})
+	});
+
+});
+
+
+
