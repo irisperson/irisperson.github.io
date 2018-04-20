460323110a2d3d5939682028513f7b6624335b28
irisperson <36912709+irisperson@users.noreply.github.com>
2018-04-19 21:39:16 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,17 @@ $(document).ready(function()  {
 
 
 	$(".picture").click(function() {
+		 // jQuery Function Number #1 //
 		$(this).addClass("pic-click");
+		 // jQuery Function Number #2 //
 		$("#overlay").show();
+		 // jQuery Function Number #3 //
 		$("#overlay").click(function(){
+			 // jQuery Function Number #4 //
 			$(this).hide();
+			 // jQuery Function Number #5 //
 			$(".picture").removeClass("pic-click");
+			 // jQuery Function Number #6 //
 		})
 	});
 
